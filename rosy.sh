1671116630d42d990dc2bb6165d1d445d9534e62
Hugo Pacheco <hpacheco@gmail.com>
2019-07-11 14:43:01 +0000

    roshask

--- rosy.sh
+++ rosy.sh
@@ -4,10 +4,11 @@ cwd=$(pwd)
 
 source base.sh
 
-run roshask/ git pull
-run roshask/  git submodule update --recursive --remote
-run gloss-window/ git pull
-run rosy/ git pull
-run codeworld-api-gloss/ git pull
+run . git submodule update --recursive --remote
+#run roshask/ git pull
+#run roshask/  git submodule update --recursive --remote
+#run gloss-window/ git pull
+#run rosy/ git pull
+#run codeworld-api-gloss/ git pull
 run . cabal install --ghcjs roshask/ roshask/msgs/* gloss-window/ rosy/ rosy-base/ codeworld-api-gloss/
 run rosy/ cabal haddock
